730d22daa3aa508b5c70000773cf6b2a22ffd700
Rafly Maulana <mraflymaulana@gmail.com>
2023-05-04 19:29:35 +0700

    Extend env variable

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,4 +4,11 @@ export declare global {
             initPopupWidget: (options: { url: string }) => void;
         };
     }
+
+    declare namespace NodeJS {
+        interface ProcessEnv {
+            GHOST_BLOG_URL: string;
+            GHOST_BLOG_CONTENT_API_KEY: string;
+        }
+    }
 }
